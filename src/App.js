757f669e8c82d6305705d432bda5e02cc50b4f2a
mahmoud-pc <mahmoud.selem@te.eg>
2020-03-20 15:11:12 +0200

    lesson two

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+import React, { Component } from 'react'
+import './App.css';
+class App extends Component {
+  state = {
+    count:0
+  }
+
+  increment = () => {
+  this.setState((state)=>({count:state.count+1}))
+  }
+  decrement = () => {
+    this.setState({count:this.state.count-1})
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment}> + </button>
+        <div>
+          {this.state.count}
+        </div>
+        <button onClick={this.decrement}> - </button>
+      </div>
+    )
+  }
 }
 
 export default App;
